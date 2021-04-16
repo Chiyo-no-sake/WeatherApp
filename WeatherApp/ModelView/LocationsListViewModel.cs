be05000da7d6704059b0bd662799d165581896d5
Luca Pasini <luca.pasini.26@gmail.com>
2021-04-16 20:38:27 +0200

    Project setup and template completed

---
+++ WeatherApp/ModelView/LocationsListViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using WeatherApp.Model;
+
+namespace WeatherApp.ModelView
+{
+    internal class LocationsListViewModel : BaseViewModel
+    {
+        private ObservableCollection<UserLocation> _entries;
+        public ObservableCollection<UserLocation> Entries
+        {
+            get => _entries;
+            set
+            {
+                _entries = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public LocationsListViewModel()
+        {
+            FillTestLocations(15);
+        }
+
+        private void FillTestLocations(int locationCount)
+        {
+            Entries = new ObservableCollection<UserLocation>();
+            for (var i = 0; i < locationCount; i++)
+            {
+                Entries.Add(new UserLocation{ID=i, Name = "Location #" + i});
+            }
+        }
+    }
+}
---
+++ WeatherApp/View/LocationsListPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using WeatherApp.ModelView;
+using Xamarin.Forms;
+
+namespace WeatherApp.View
+{
+    public partial class LocationsListPage : ContentPage
+    {
+        public LocationsListPage()
+        {
+            InitializeComponent();
+            BindingContext = new LocationsListViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        void OnItemAdded(object sender, EventArgs e)
+        {
+            DisplayAlert("Item Added", "An item was added to the list", "OK");
+        }
+
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem is null)
+                return;
+
+            Navigation.PushAsync(new LocationDetailsPage()
+            {
+                BindingContext = e.SelectedItem as Model.UserLocation
+            });
+        }
+    }
+}
